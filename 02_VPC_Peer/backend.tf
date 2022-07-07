654afab865563d3d7039e4a28f4f0fc3de6a4d86
manasavinay26 <99788387+manasavinay26@users.noreply.github.com>
2022-07-07 11:36:10 +0530

    Update backend.tf

--- 02_VPC_Peer/backend.tf
+++ 02_VPC_Peer/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     encrypt        = true
     bucket         = "tf-legacy-backend-prod-terraform-state-bucket-1"
-    key            = "tf-legacy-backend/vpc_peer/terraform.tfstate"
+    key            = ""
   }
 }
