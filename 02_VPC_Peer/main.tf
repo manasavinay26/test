d9eac789e0b20d820bdd3d3106b6bc578763986e
manasavinay26 <99788387+manasavinay26@users.noreply.github.com>
2022-07-07 12:30:06 +0530

    Update main.tf

--- 02_VPC_Peer/main.tf
+++ 02_VPC_Peer/main.tf
@@ -7,26 +7,27 @@ resource "aws_vpc_peering_connection" "shared" {
   peer_vpc_id   = "vpc-0860d8c9ca00e66b7"
   vpc_id        = "vpc-0cd3098624fdce91e"
   
+  
   tags = {
-      "Name"     = "LegacyQA to sharedMain"
-      "Resource" = "aws_vpc_peering_connection.shared"
-    }
+    "Name"     = "LegacyQA to sharedMain"
+    "Resource" = "aws_vpc_peering_connection.shared"
+   }
 }
-resource "aws_route" "private-route-legacy-qa-1" {
+resource "aws_route" "private_route_legacy_qa_1" {
   route_table_id            = "rtb-0861599cb0cdff558"
   destination_cidr_block    = "10.168.8.0/23"
   vpc_peering_connection_id = aws_vpc_peering_connection.shared.id
   depends_on                = [aws_vpc_peering_connection.shared]
 }
 
-resource "aws_route" "private-route-legacy-qa-2" {
+resource "aws_route" "private_route_legacy_qa_2" {
   route_table_id            = "rtb-0dcd4eef0b606445b"
   destination_cidr_block    = "10.168.8.0/23"
   vpc_peering_connection_id = aws_vpc_peering_connection.shared.id
   depends_on                = [aws_vpc_peering_connection.shared]
 }
 
-resource "aws_route" "private-route-legacy-qa-3" {
+resource "aws_route" "private_route_legacy_qa_3" {
   route_table_id            = "rtb-0b8d504e4db94873f"
   destination_cidr_block    = "10.168.8.0/23"
   vpc_peering_connection_id = aws_vpc_peering_connection.shared.id
