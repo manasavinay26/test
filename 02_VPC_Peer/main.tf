8e3bb49c09bf247a55705bdca29272ef8c58c220
manasavinay26 <99788387+manasavinay26@users.noreply.github.com>
2022-07-07 10:50:09 +0530

    Create main.tf

---
+++ 02_VPC_Peer/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test_VPC"
+  }
+}
